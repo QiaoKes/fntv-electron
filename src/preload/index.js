5d03ebda4782a725c6f655d22a7d173a7ef4cee3
tracing <2281582306@qq.com>
2025-08-26 12:16:00 +0800

    [fix] log bug

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 const path = require('path');
-const { contextBridge } = require('electron');
 
 const { runHooks } = require('./core/hooks.js');
 
 // 导入渲染进程日志模块
 const preloadLogger = require('./logger');
 
-// 暴露日志接口到渲染进程
-contextBridge.exposeInMainWorld('log', preloadLogger);
+// 由于 contextIsolation: false，直接在全局对象上暴露日志接口
+global.log = preloadLogger;
+global.logger = preloadLogger;
 
-// 为了向后兼容，也暴露为logger
-contextBridge.exposeInMainWorld('logger', preloadLogger);
+// 如果在浏览器环境中，也暴露到window对象
+if (typeof window !== 'undefined') {
+    window.log = preloadLogger;
+    window.logger = preloadLogger;
+}
 
 // 自动加载插件
 const pluginsDir = path.join(__dirname, 'plugins');
@@ -22,6 +25,12 @@ fs.readdirSync(pluginsDir).forEach(file => {
 });
 
 function initInjector() {
+    // 由于 contextIsolation: false，在DOM ready时暴露到window对象
+    if (typeof window !== 'undefined') {
+        window.log = preloadLogger;
+        window.logger = preloadLogger;
+    }
+    
     if (document.readyState !== 'loading') {
         runHooks('onReady');
     } else {
