5f8123a7dd676216deb7c2ba32f58e68e426b99a
tracing <2281582306@qq.com>
2025-09-04 22:21:36 +0800

    [add] somefunc

--- src/preload/core/utils.ts
+++ src/preload/core/utils.ts
@@ -1,6 +1,24 @@
 // preload/core/utils.ts
 import type { Utils } from './types';
 
+// 检查当前页面是否最后一层(电影或剧集页面或者其他页面)
+function checkFinalPageUrl() {
+    const url = window.location.href;
+    return url.includes('/v/movie/') || url.includes('/v/tv/episode/') || url.includes('/v/other/');
+}
+
+// 检查当前页面是否为季页面
+function checkSeasonPageUrl() {
+    const url = window.location.href;
+    return url.includes('/v/tv/season/');
+}
+
+// 检查当前页面是否为剧集页面
+function checkTVPageUrl() {
+    const url = window.location.href;
+    return url.includes('/v/tv/');
+}
+
 function getCookie(name: string): string | null {
     const cookies = document.cookie.split(';');
     const nameEQ = name + '=';
@@ -18,5 +36,5 @@ const utils: Utils = {
     getCookie,
 };
 
-export { getCookie };
+export { getCookie, checkFinalPageUrl, checkSeasonPageUrl, checkTVPageUrl };
 export default utils;
