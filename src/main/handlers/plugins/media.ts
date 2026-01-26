bf5a656f94c660b9f3f17f793deba09dec82e4b6
tracing <2281582306@qq.com>
2026-01-26 15:48:52 +0800

    [add] support redit url & mpv net timeout

--- src/main/handlers/plugins/media.ts
+++ src/main/handlers/plugins/media.ts
@@ -313,6 +313,7 @@ async function handlePlayMovie(event: IpcMainEvent, { id, token }: PlayRequest):
         // },
         extraArgs: [
             '--force-window=immediate',
+            '--network-timeout=180'
             // "--user-agent=Lavf/59.27.100",
         ],
         debug: true,
